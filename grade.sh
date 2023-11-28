a91cc8ae2cce5038c288e386fb5992b60ffde2bd
Ricardo Bomeny <50306275+rbmny@users.noreply.github.com>
2023-11-28 10:13:17 -0800

    Autograder upload

--- grade.sh
+++ grade.sh
@@ -8,6 +8,31 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+PATHJ=$(find student-submission -name "ListExamples.java")
+
+if [ $? -ne 00 ]
+then
+  echo "No Java files found"
+  exit 1
+fi
+  cp -r "$PATHJ" TestListExamples.java grading-area
+
+javac -cp $CPATH grading-area/ListExamples.java grading-area/TestListExamples.java
+
+if [ $? -ne 00 ]
+then
+  echo "Java not Compiled"
+  exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore grading-area/TestListExamples > Junit-output.txt
+JUNIT=$(grep "Tests run" Junit-output.txt | grep -o '[0-9]\+' | awk 'NR==1')
+JUNIT2=$(grep "Tests run" Junit-output.txt | grep -o '[0-9]\+' | awk 'NR==2')
+echo $JUNIT/$JUNIT2
+#TESTT=$( $JUNIT | grep -o '[0-9]\+')
+
+
+
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
---
+++ test.sh
@@ -0,0 +1,1 @@
+bash grade.sh https://github.com/ucsd-cse15l-w23/lab3.git
